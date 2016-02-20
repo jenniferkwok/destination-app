74d92c411b5a6d1399ec0eb37bde4bb9c56f7a8f
jenniferkwok <jenwkwok@gmail.com>
2016-02-19 17:23:11 -0800

    responsive

--- public/scripts.js
+++ public/scripts.js
@@ -2,10 +2,11 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-	$(function(){
-		$("#page-wrap").wrapInner("<table cellspacing='30'><tr>");
-		$(".post").wrap("<td>");
-	});
+// 	$(function(){
+// 		$("#page-wrap").wrapInner("<table cellspacing='30'><tr>");
+
+// 		$(".post").wrap("<td>");
+// 	});
 		pageload();
 		$('.modal').appendTo("body");
 //SIGNUP
@@ -121,33 +122,7 @@ function userFav(context){
 		}
 	});
 }
-// LOGO EVERYWHERE
-            window.onLoad = Prep();
-            
-            function Prep(){
-                window_Height = window.innerHeight;
-                window_Width = window.innerWidth;
-                
-                image_Element = document.getElementById("pin");
-                image_Height = image_Element.clientHeight;
-                image_Width = image_Element.clientWidth;
-                
-                availSpace_V = window_Height - image_Height;
-                availSpace_H = window_Width - image_Width;
-                
-                var changeInterval = 1000; // Time has to be in miliseconds. So, 3000 is 3 seconds
-                setInterval(moveImage, changeInterval);
-            }
-            
-            function moveImage(){
-                var randNum_V = Math.round(Math.random() * availSpace_V);
-                var randNum_H = Math.round(Math.random() * availSpace_H);
-                
-                image_Element.style.top = randNum_V + "px";
-                image_Element.style.left = randNum_H + "px";
-            }
-
-// RUN!
+// LOGO EYERYWHERE
 function moveDiv() {
     var $span = $("#pin");
     
